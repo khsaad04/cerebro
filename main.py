7f0568a16a1824a6c47ef2c9f78c5d101e909ecc
Replit user <>
2023-07-25 14:08:46 +0000

    ok
    done
    
    Rebase

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+from flask import Flask
+from threading import Thread
 import os
 from threading import Thread
 
@@ -24,6 +26,25 @@ def run():
 def keep_alive():
     server = Thread(target=run)
     server.start()
+
+
+app = Flask("")
+
+
+@app.route("/")
+def main():
+    return "Your Bot Is Ready"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8000)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
+token = os.environ.get("DISCORD_TOKEN")
 
 
 async def main():
