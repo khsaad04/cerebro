9ac3e053f8a5f61625432b1091483a81030a9c95
khsaad04 <khsaad0404@gmail.com>
2023-07-25 19:57:13 +0600

    added keep alive

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+from threading import Thread
+
+from flask import Flask
+
+app = Flask("")
+
+
+@app.route("/")
+def main():
+    return "Your Bot Is Ready"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8000)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import os
 from discord.utils import setup_logging
 
 from core import Bot
+from keep_alive import keep_alive
 
 token = os.environ.get("TOKEN")
 
 
 async def main():
     setup_logging()
+    keep_alive()
     async with Bot() as bot:
         await bot.start(token=token, reconnect=True)
 
