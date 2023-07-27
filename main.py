99ba3842fcee362e2ec4f1c29959f704845535b7
khsaad04 <khsaad0404@gmail.com>
2023-07-27 16:46:45 +0600

    Removed multi-threading and flask server

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 import asyncio
 import os
-from threading import Thread
 
 from discord.utils import setup_logging
-from flask import Flask
 
 from core import Bot
 
 token = os.environ.get("TOKEN")
-app = Flask("")
-
-
-@app.route("/")
-def main():
-    return "Your Bot Is Ready"
-
-
-def run():
-    app.run(host="0.0.0.0", port=8000)
-
-
-def keep_alive():
-    server = Thread(target=run)
-    server.start()
 
 
 async def main():
     setup_logging()
-    keep_alive()
     async with Bot() as bot:
         await bot.start(token=token, reconnect=True)
 
