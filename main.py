4a6691632fc7fafe087fff5aad844dbd432c3cd3
khsaad04 <khsaad0404@gmail.com>
2023-08-02 15:18:28 +0600

    merge conflict fixed

--- main.py
+++ main.py
@@ -5,7 +5,23 @@ from discord.utils import setup_logging
 
 from core import Bot
 
-token = os.environ.get("TOKEN")
+token = os.environ.get("DISCORD_TOKEN")
+
+app = Flask("")
+
+
+@app.route("/")
+def main():
+    return "Your Bot Is Ready"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8000)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
 
 
 async def main():
